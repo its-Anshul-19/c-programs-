cd9c8a3d23911456d9d89701eda90642373c772a
Anshul Kumar <anshulk2012btechcse2025@kccitm.edu.in>
2022-07-03 15:26:25 +0530

    type_cast4-with-float.cpp

---
+++ type-cast4.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int a;
+    char c;
+    a=1990;
+    c=a;
+    cout << c << endl;
+    c='y';
+    a=c;
+    cout << a << endl;
+
+    float f = 1.2;
+    a = f;
+    cout << a << endl;
+
+
+}
