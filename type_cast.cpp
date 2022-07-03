2d9348ec2622cb6a32b94ce8708508188715bdb5
Anshul Kumar <anshulk2012btechcse2025@kccitm.edu.in>
2022-07-03 14:50:14 +0530

    type_cast.cpp

---
+++ type_cast.cpp
@@ -0,0 +1,16 @@
+//type cast to print character
+-----------------------------------------------------------------------------------------
+
+
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int a;
+    char c;
+    a=100;
+    c=a;
+    cout << c << endl;
+
+}
