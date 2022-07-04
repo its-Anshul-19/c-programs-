7dcf2f19b24c90043a642bb8128fa162c058d312
Anshul Kumar <anshulk2012btechcse2025@kccitm.edu.in>
2022-07-04 20:07:24 +0530

    EVEN-ODD.cpp

---
+++ EVEN_ODD.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int n;
+    cout <<" enter  the number" << endl;
+
+    cin >> n;
+
+    if (n%2==0){
+        cout << "THE NUMBER IS:";
+        cout << "EVEN" << endl;
+
+    } else {
+        cout << "THE NUMBER IS:";
+        cout << "ODD" << endl;
+    }
+}
+
