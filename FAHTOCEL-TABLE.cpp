e5f293bf316b6202f94356956ca3ccd16cd15156
its-Anshul-18 <108014798+its-Anshul-18@users.noreply.github.com>
2022-07-05 20:28:25 +0530

    Create FAHTOCEL-TABLE.cpp

---
+++ FAHTOCEL-TABLE.cpp
@@ -0,0 +1,19 @@
+/*FAHRENHEIT TO CELSIUS TABLE*/
+
+-----------------------------------------------------------------------
+  #include <iostream>
+using namespace std;
+
+int main(){
+    int S,E,W;
+    cin >> S >> E>> W;
+    int C;
+    
+    while (S<=E){
+        C=5.0/9*(S-32);
+        cout << S << "\t" << C<<endl;
+        S=S+W;
+    }
+}
+
+  
