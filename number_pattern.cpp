a6147906cfb7b990cdc026d4d37538da48c8acc7
its-Anshul-18 <108014798+its-Anshul-18@users.noreply.github.com>
2022-07-05 20:37:36 +0530

    Create number_pattern.cpp

---
+++ number_pattern.cpp
@@ -0,0 +1,26 @@
+/*LECTURE 2 NUMBER PATTER 1 */
+------------------------------------------------------
+  #include <iostream>
+using namespace std;
+
+int main(){
+    
+    int n;
+    //cout <<"Enter n" << endl;
+    
+    cin >> n;
+    
+    int i=1;
+    
+    while (i<=n){
+        int j=1;
+        int var=i;
+        while(j<=i){
+            cout<<var;
+            j=j+1;
+            var=var+1;
+        }
+        cout<<endl;
+        i=i+1;
+    }
+}
